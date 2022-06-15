9fc1abc5ffaa7093f63b8ffb06ac472731a555c9
vgusain <105430438+vgusain@users.noreply.github.com>
2022-06-15 17:03:28 +0530

    Do changes after this commit for nodeTraining

--- controller/signupController.js
+++ controller/signupController.js
@@ -4,31 +4,8 @@ const userService = require('../service/userService');
 const transactionService = require('../service/transactionService');
 
 router.post('/signup', async function (req, res) {
-    var user =  createUserFromRequest(req);
-    var isNewUserCreated =  await userService.createNewUser(user);
-    if(isNewUserCreated) {
-        var refererUser = await userService.getUserByReferalId(user.refBy);
-        if(refererUser != null) {
-            console.log("Referrer User id: " + refererUser.userId); 
-            transactionService.addBonusForReferrer(refererUser);
-            transactionService.addBonusForNewUser(user.mob_num);
-            return res.send("New User Created for mob_num : " + user.mob_num);
-        } else {
-            const message = "Invalid Refferal Code";
-            return res.send(message);
-        }
-    }
     
 });
 
-function createUserFromRequest(req) {
-    return  {
-        userName : req.body.user_name,
-        email : req.body.email,
-        refBy : req.body.refBy,
-        auto_password : req.body.auto_password,
-        mob_num : req.body.mob_num
-    };
-}
 
 module.exports = router;
--- index.js
+++ index.js
@@ -12,27 +12,6 @@ app.use(bodyParser.json());
 
 app.use("/", signUpController)
 
-/*
-app.post('/api/signup', async function (req, res) {
-    var userName = req.body.user_name;
-    var email = req.body.email;
-    var refBy = req.body.refBy;
-    var auto_password = req.body.auto_password;
-    var mob_num = req.body.mob_num;
-    var newUser =  await signUpService.createNewUser(userName, email, mob_num, auto_password);
-    var refererUser = await userService.getUserByReferalId(refBy);
-    if(refererUser != null) {
-      console.log("Referrer User id: " + refererUser.id); 
-      transactionService.addBonusForReferer(refererUser);
-      transactionService.addBonusToNewUser(newUser);
-      return res.send("New User Created username, email " + userName + " " + email + " " + newUser);
-    } else {
-      const message = "Invalid Refferal Code";
-      return res.send(message);
-    }
-});
-*/
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
